f39cb3f64ac226830e6423cffcceee2dbd2cdc60
Ajay <139728870+Ajay110702@users.noreply.github.com>
2026-04-14 23:04:47 +0530

    Update utils.jsx

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -4,5 +4,5 @@ export const notify=(message,type)=>{
    toast[type](message);
    
 }
-export const API_URL='http://localhost:3000';
+export const API_URL='https://task-manager-api-omega-ten.vercel.app';
 
